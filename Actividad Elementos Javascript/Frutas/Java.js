1a5a426de94212de95f2d8aae95d333b53e0ac6d
AriannaNoce <148570864+AriannaNoce@users.noreply.github.com>
2023-10-20 13:01:45 -0300

    Add files via upload

---
+++ Actividad Elementos Javascript/Frutas/Java.js
@@ -0,0 +1,41 @@
+var frutas = ["manzana","frutilla","kiwi"];
+
+console.log (frutas [0])
+console.log (frutas [1])
+console.log (frutas [2])
+
+//imprimir cada fruta del array
+
+function agregarFruta (fruta){
+    frutas.push("Banana")
+} 
+
+agregarFruta();
+console.log (frutas)
+
+// Agregar fruta
+
+for (indice = 0; indice < 2 ; indice++){
+    console.log (frutas [0])
+    console.log (frutas [1])
+    console.log (frutas [2])
+    console.log (frutas [3]);
+    }
+
+
+ // bucle
+
+function longitud (){
+    frutas [frutas.length-1];
+ }
+
+ if (longitud %2){
+    console.log ("Es Par");
+   }
+   else {
+       console.log ("Es Impar")
+   }
+
+longitud(frutas);
+
+ //longitud
---
+++ Actividad Elementos Javascript/Par o Impar/Javascript.js
@@ -0,0 +1,16 @@
+// PRINICPIO EJERCICIO PAR O IMPAR
+contador = 0;
+
+
+while (contador < 100){
+    contador ++;
+    esPar = contador%2;
+if (esPar == 0){
+ console.log ("El numero " + contador + " Es Par");
+}
+else {
+    console.log ("El numero " + contador + " Es Impar")
+}
+}
+
+// FIN EJERCICIO PAR O IMPAR
